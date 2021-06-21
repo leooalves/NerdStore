252c8f6adf8ef1e64f84b059766a059d5e158418
Leonardo Oliveira Alves <leonardo.oliveiralves@gmail.com>
2021-06-21 14:40:34 -0300

    adicionado queries no contexto de vendas

---
+++ src/Services/Vendas/NerdStore.Vendas.Api/Application/Queries/IPedidoQueries.cs
@@ -0,0 +1,13 @@
+﻿using NerdStore.Vendas.Api.Application.Queries.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Api.Application.Queries
+{
+    public interface IPedidoQueries
+    {
+        Task<CarrinhoViewModel> ObterCarrinhoCliente(Guid clienteId);
+        Task<IEnumerable<PedidoViewModel>> ObterPedidosCliente(Guid clienteId);
+    }
+}
---
+++ src/Services/Vendas/NerdStore.Vendas.Api/Application/Queries/ViewModels/CarrinhoItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NerdStore.Vendas.Api.Application.Queries.ViewModels
+{
+    public class CarrinhoItemViewModel
+    {
+        public Guid ProdutoId { get; set; }
+        public string ProdutoNome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public decimal ValorTotal { get; set; }
+
+    }
+}
